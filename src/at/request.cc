6b631a5b085646a7490545e655299302d15dfae7
Paolo Galeone <nessuno@nerdz.eu>
2018-02-23 17:56:01 +0000

    Request: wrap curlpp::LibcurlRuntimeError

--- src/at/request.cc
+++ src/at/request.cc
@@ -27,17 +27,22 @@ json Request::get(std::string url) { return json::parse(_get(url)); }
 std::string Request::_get(std::string url)
 {
     curlpp::Easy req;
-
-    req.setOpt(Url(url));
-    req.setOpt(FollowLocation(true));
-    req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
-
-    if (!_headers.empty()) {
-        req.setOpt(HttpHeader(_headers));
-    }
     std::ostringstream stream;
-    req.setOpt(WriteStream(&stream));
-    req.perform();
+    try {
+        req.setOpt(Url(url));
+        req.setOpt(FollowLocation(true));
+        req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
+
+        if (!_headers.empty()) {
+            req.setOpt(HttpHeader(_headers));
+        }
+
+        req.setOpt(WriteStream(&stream));
+        req.perform();
+    }
+    catch (const curlpp::LibcurlRuntimeError& e) {
+        throw server_error(e.what());
+    }
 
     long code = curlpp::infos::ResponseCode::get(req);
     if (code == 200L) {
@@ -53,29 +58,33 @@ std::string Request::_get(std::string url)
 json Request::post(std::string url, json params)
 {
     curlpp::Easy req;
-
-    req.setOpt(Url(url));
-    req.setOpt(FollowLocation(true));
-    req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
-
-    std::list<std::string> headers({"Content-Type: application/json"});
-    if (!_headers.empty()) {
-        headers.insert(headers.end(), _headers.begin(), _headers.end());
-    }
-    req.setOpt(HttpHeader(headers));
-
-    // Write params to a stream and use the stream to
-    // convert to string
     std::ostringstream stream;
-    stream << params;
-    std::string data = stream.str();
-    req.setOpt(PostFields(data));
-    req.setOpt(PostFieldSize(data.length()));
+    try {
+        req.setOpt(Url(url));
+        req.setOpt(FollowLocation(true));
+        req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
 
-    stream.str("");
-    stream.clear();
-    req.setOpt(WriteStream(&stream));
-    req.perform();
+        std::list<std::string> headers({"Content-Type: application/json"});
+        if (!_headers.empty()) {
+            headers.insert(headers.end(), _headers.begin(), _headers.end());
+        }
+        req.setOpt(HttpHeader(headers));
+
+        // Write params to a stream and use the stream to
+        // convert to string
+        stream << params;
+        std::string data = stream.str();
+        req.setOpt(PostFields(data));
+        req.setOpt(PostFieldSize(data.length()));
+
+        stream.str("");
+        stream.clear();
+        req.setOpt(WriteStream(&stream));
+        req.perform();
+    }
+    catch (const curlpp::LibcurlRuntimeError& e) {
+        throw server_error(e.what());
+    }
 
     long code = curlpp::infos::ResponseCode::get(req);
     if (code == 200L) {
@@ -92,34 +101,38 @@ json Request::post(std::string url,
                    std::vector<std::pair<std::string, std::string>> params)
 {
     curlpp::Easy req;
-
-    req.setOpt(Url(url));
-    req.setOpt(FollowLocation(true));
-    req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
-
-    std::list<std::string> headers(
-        {"Content-Type: application/x-www-form-urlencoded"});
-    if (!_headers.empty()) {
-        headers.insert(headers.end(), _headers.begin(), _headers.end());
-    }
-    req.setOpt(HttpHeader(headers));
-
     std::ostringstream stream;
-    for (auto& pair : params) {
-        std::string key(pair.first);
-        std::string value(pair.second);
-        stream << key << "=" << curlpp::escape(value) << "&";
+    try {
+        req.setOpt(Url(url));
+        req.setOpt(FollowLocation(true));
+        req.setOpt(SslVersion(CURL_SSLVERSION_TLSv1_2));
+
+        std::list<std::string> headers(
+            {"Content-Type: application/x-www-form-urlencoded"});
+        if (!_headers.empty()) {
+            headers.insert(headers.end(), _headers.begin(), _headers.end());
+        }
+        req.setOpt(HttpHeader(headers));
+
+        for (auto& pair : params) {
+            std::string key(pair.first);
+            std::string value(pair.second);
+            stream << key << "=" << curlpp::escape(value) << "&";
+        }
+
+        auto postFields = stream.str();
+        postFields.pop_back();  // remove last &
+        req.setOpt(PostFields(postFields));
+        req.setOpt(PostFieldSize(postFields.length()));
+
+        stream.str("");
+        stream.clear();
+        req.setOpt(WriteStream(&stream));
+        req.perform();
     }
-
-    auto postFields = stream.str();
-    postFields.pop_back();  // remove last &
-    req.setOpt(PostFields(postFields));
-    req.setOpt(PostFieldSize(postFields.length()));
-
-    stream.str("");
-    stream.clear();
-    req.setOpt(WriteStream(&stream));
-    req.perform();
+    catch (const curlpp::LibcurlRuntimeError& e) {
+        throw server_error(e.what());
+    }
 
     long code = curlpp::infos::ResponseCode::get(req);
     if (code == 200L) {
